01f3fadd72d1a41b6a0e4d7c24d6d8515efdf3aa
ZracheSs | xyZ <85443178+ZracheSs-xyZ@users.noreply.github.com>
2021-07-18 16:30:45 +0100

    Update SecretStorage.py

--- SecretStorage.py
+++ SecretStorage.py
@@ -1,5 +1,9 @@
 ScholarsDict = {
-    # 'DiscordID' : ['DiscordName', 'EthWalletAddress', 'EthPrivateKey']
+    # 'DiscordID' : ['DiscordName', 'RoninWalletAddress', 'RoninPrivateKey']
+
+    # The RoninWalletAddress should start with 0x instead of ronin:
+    # The RoninPrivateKey should be parsed without the 0x
+    
     # The following are placeholders...
     '829972850726746576' : ['Scholars1 | xyZ', '0x22Aw4449332059577e6C48jD0811783A7d852abF','4r512g58s9g1j23365h8ef1h2g55c8nmu525w8d4fg3h2c115qf369s0ew4e2d48'],
     '803436637857794788' : ['Scholars2 | xyZ', '0xj2FAD2365720zFb8j06450bE3b58a149676f9E93','m1e02qrc115qf369s0ew4e2g50wj5525w8d4fg3h2c115c8nmu51j23365h8eh1n']
