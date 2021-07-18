d1163ff68c7e083a2c2fd168fad2303eabd31372
ZracheSs | xyZ <85443178+ZracheSs-xyZ@users.noreply.github.com>
2021-07-18 16:28:04 +0100

    Add files via upload

---
+++ SecretStorage.py
@@ -0,0 +1,9 @@
+ScholarsDict = {
+    # 'DiscordID' : ['DiscordName', 'EthWalletAddress', 'EthPrivateKey']
+    # The following are placeholders...
+    '829972850726746576' : ['Scholars1 | xyZ', '0x22Aw4449332059577e6C48jD0811783A7d852abF','4r512g58s9g1j23365h8ef1h2g55c8nmu525w8d4fg3h2c115qf369s0ew4e2d48'],
+    '803436637857794788' : ['Scholars2 | xyZ', '0xj2FAD2365720zFb8j06450bE3b58a149676f9E93','m1e02qrc115qf369s0ew4e2g50wj5525w8d4fg3h2c115c8nmu51j23365h8eh1n']
+
+}
+# Put Your Discord Bot Token Here
+DiscordBotToken = 'DFV9NTAyMDfDvdD1MDI0ODs7.HK-_4S.9dmfxFGtg_2s1-aMk-FCeDFsQfdV'
